f23c48ae0de2262341168a6f7754f2cdbfa94d1b
Maarten Donkersloot <d225480@edu.rocwb.nl>
2017-09-29 09:04:44 +0200

    adding stuff

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.layoutbasictop')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center">
+
+                <div class="list-group list-group-horizontal">
+                    <a href="#" class="list-group-item active">sales</a>
+                    <a href="#" class="list-group-item">finance</a>
+                    <a href="#" class="list-group-item">development</a>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+@endsection
+@extends('layouts.layoutbasicbottom')
