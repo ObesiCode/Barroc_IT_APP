b1d86192cfec228d128b7d0af613cf37ab597bed
Maarten Donkersloot <d225480@edu.rocwb.nl>
2017-09-29 09:26:02 +0200

    pushing noell

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,12 +3,13 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center">
+            <div class="main">
 
                 <div class="list-group list-group-horizontal">
-                    <a href="#" class="list-group-item active">sales</a>
-                    <a href="#" class="list-group-item">finance</a>
-                    <a href="#" class="list-group-item">development</a>
+                    <div class="col-sm-4 text-center list-group-item"><a href="#" class="">sales</a></div>
+                    <div class="col-sm-4 text-center list-group-item"><a href="#" class="">finance</a></div>
+                    <div class="col-sm-4 text-center list-group-item"><a href="#" class="">development</a></div>
+
 
                 </div>
 
