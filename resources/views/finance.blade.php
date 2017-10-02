1bc046e46f1abcd9cb9512ffe22aea6807ac147a
Unknown <d230195@edu.rocwb.nl>
2017-10-02 10:34:01 +0200

    finance

--- resources/views/finance.blade.php
+++ resources/views/finance.blade.php
@@ -1,73 +1,49 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Finance</title>
-</head>
-<body>
-<form action="">
 
-    {{csrf_field()}}
+@extends('layouts.layoutbasictop')
 
-<div class="form-group">
-    <div class="form-control">
-        <label for="invoice_id"> Invoice Id</label>
-        <input type="text" id="invoice_id">
+@section('content')
+    <div class="input">
+        <div class="panel-group">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <form action="">
+                        <div class="form-group col-xs-4">
+
+                        <label for="invoice_id"> Invoice Id</label>
+                        <input type="text" id="invoice_id">
+
+                        <label for="Doa"> Date Of Action</label>
+                        <input type="text" id="Doa">
+                        </div>
+                    </form>
+                </div>
+            </div>
+
     </div>
-</div>
-<div class="form-group">
-    <div class="form-control">
-        <label for="offer-number">offer-number</label>
-        <input type="text" id="offer-number">
-    </div>
-</div>
-<div class="form-group">
-    <div class="form-control">
-        <label for="Doa"> Date Of Action</label>
-        <input type="text" id="Doa">
-    </div>
-</div>
-<div class="form-group">
-    <div class="form-control">
-        <label for="Payment">Payment Data</label>
-        <input type="text" id="Payment">
-    </div>
-</div>
-<div class="form-group">
-    <div class="form-control">
-        <label for="Cus_id">Customer ID</label>
-        <input type="text" id="Cus_id">
-    </div>
-</div>
-<div class="form-group">
-    <div class="form-control">
-        <label for="Cus_name">Customer Name</label>
-        <input type="text" id="Cus_name">
-    </div>
-</div>
-<div class="form-group">
-    <div class="form-control">
-        <label for="Email"> Email</label>
-        <input type="email" id="Email">
-    </div>
-</div>
-<div class="form-group">
-    <div class="form-control">
-        <label for="Phone"> Phone Number</label>
-        <input type="text">
-    </div>
-</div>
+<div class="col-sm-4" id="customertable">
+    <form action="">
+        <table class="table table-bordered" id="tableclass">
+            <thead>
+            <tr>
+                <th id="tabletoptext">Invoice ID</th>
+                <th id="tabletoptext">CustomerID</th>
+                <th id="tabletoptext">Name</th>
+                <th id="tabletoptext">Edit</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td><input type="text" name="IID" id="inputtext"></td>
+                <td><input type="text" name="cusID" id="inputtext"></td>
+                <td><input type="text" name="cusName" id="inputtext"></td>
+                <td><input type="checkbox" name="Edit" id="inputtext"></td>
+            </tr>
 
 
 
-<div class="form-group">
-    <div class="form-control">
-        <input type="submit">
-    </div>
+            </tbody>
+        </table>
+        <input type="submit" id="submitbutton" placeholder="Send">
+    </form>
 </div>
-</form>
-</body>
-</html>
+
