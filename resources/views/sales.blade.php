2c692f29b36c808afff3da6761544e3964746605
Maarten Donkersloot <d225480@edu.rocwb.nl>
2017-10-02 12:03:42 +0200

    sales beyond thunderdome

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -3,14 +3,15 @@
 @section('content')
 
 <div class="main">
-    <div class="col-sm-4">
+    <div class="col-sm-4" id="testing">
     <div class="panel panel-default" id="salespanel">
         <div class="panel-heading">
             <h1>Customer</h1>
         </div>
         <div class="panel-body" id="">
             <form action="">
-                 <div class="form-group col-xs-4">
+                {{csrf_field()}}
+                 <div class="form-group col-xs-4" >
                      <label for="customer" id="labeltext">customer</label>
                      <input type="text" class="customer" id="salesinput">
 
@@ -54,11 +55,7 @@
                     <label for="doa" id="labeltext">date of action</label>
                     <input readonly type="text" class="doa" id="salesinput" value="{{\Carbon\Carbon::now()->format('l j F Y ')}}">
 
-                    <label for="la" id="labeltext">Last action</label>
-                    <input type="text" class="la" id="salesinput">
-
-                    <label for="na" id="labeltext">next action</label>
-                    <input type="text" class="na" id="salesinput">
+                   
 
                     <label for="log" id="labeltext">Log</label>
                     <input type="text" class="log" id="salesinput">
@@ -71,7 +68,38 @@
     </div>
     </div>
     <div class="col-sm-4"></div>
-    .col-sm-4
+    <div class="col-sm-4" id="testing">
+        <form action="">
+            {{csrf_field()}}
+        <table class="table table-bordered" id="tableclass">
+            <thead>
+            <tr >
+                <th id="customth"><label for="search" id="labeltext">Search</label> <input type="text" id="search" name="search" >
+
+                </th>
+                <th><input type="submit" id="submitbuttonsales"></th>
+            </tr>
+            <tr>
+                <th id="tabletoptext">invoice-id</th>
+                <th id="tabletoptext">Customer-id</th>
+                <th id="tabletoptext">name</th>
+                <th id="tabletoptext"><p>EDIT</p></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td><input type="text" name="invoicid" id="inputtext"></td>
+                <td><input type="text" name="customerid" id="inputtext"></td>
+                <td><input type="text" name="name" id="inputtext"></td>
+                <td><a href="#">EDIT</a></td>
+            </tr>
+
+
+
+            </tbody>
+        </table>
+        </form>
+    </div>
 
 
 
