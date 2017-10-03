0a58e5bcb08b649db2d114f058b1eb7aaa0e8e92
d201670 <d201670@edu.rocwb.nl>
2017-10-03 09:46:59 +0200

    developmentform

--- resources/views/development.blade.php
+++ resources/views/development.blade.php
@@ -22,56 +22,58 @@
                             {{csrf_field()}}
                             <div class="col-xs-4">
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="projectname" id="labeltext">Project name</label>
+                                    <input type="text" class="projectname" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="projectid" id="labeltext">Project-ID</label>
+                                    <input readonly type="text" class="projectid" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="customername" id="labeltext">Customer name</label>
+                                    <input type="text" class="customername" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="email" id="labeltext">Email</label>
+                                    <input type="email" class="email" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="phonenumber" id="labeltext">Phone number</label>
+                                    <input type="text" class="phonenumber" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="offertenumber" id="labeltext">Offerte number</label>
+                                    <input readonly type="number" class="offertenumber" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="log" id="labeltext">Log</label>
+                                    <input type="text" class="log" id="salesinput">
+                                </div>
+                            </div>
+                            <div class="col-xs-4">
+                                <div class="form-group">
+                                    <label for="application" id="labeltext">Application</label>
+                                    <input type="text" class="application" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="hardware" id="labeltext">Hardware</label>
+                                    <input type="text" class="hardware" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="opsystem" id="labeltext">Operating system</label>
+                                    <input type="text" class="opsystem" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="contactperson" id="labeltext">Contact person</label>
+                                    <input type="text" class="contactperson" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="lastcontact" id="labeltext">Last contact</label>
+                                    <input type="text" class="lastcontact" id="salesinput">
                                 </div>
                                 <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
-                                </div>
-                                <div class="form-group">
-                                    <label for="" id="labeltext"></label>
-                                    <input type="text" class="" id="salesinput">
+                                    <label for="nextcontact" id="labeltext">Next contact</label>
+                                    <input type="text" class="nextcontact" id="salesinput">
                                 </div>
                                 <div class="form-group">
                                 <input type="submit" class="number" id="submitbuttonsales">
