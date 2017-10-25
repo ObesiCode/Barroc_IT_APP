089a08b2f98a23e26759c67b807fe3f627947398
Unknown <d230195@edu.rocwb.nl>
2017-10-25 13:23:14 +0200

    pop

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../../../public/css/login.css">
-    <title>Login</title>
-</head>
-<body>
+@extends('layouts.app')
 
+@section('content')
 <div class="container">
     @if (count($errors) > 0)
         <div class="row">
@@ -93,6 +84,4 @@
         </div>
     </div>
 </div>
-
-</body>
-</html>
+@endsection
